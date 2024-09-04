9dfc9046b60554a2d060f309666b09731598ed66
unwontedsponn <benpaulspooner@gmail.com>
2024-09-04 09:02:24 +0100

    Escaped 3 "'"

--- app/sections/Welcome.tsx
+++ app/sections/Welcome.tsx
@@ -7,7 +7,7 @@ export default function Welcome() {
         <div className="w-[85vw] max-w-4xl p-4 text-center text-black">
           <h1 className="text-xl font-bold">WELCOME TO ARISE</h1>
           <h2>inspiring, Down to earth & playful breathwork sessions THAT BENEFIT YOUR ENTIRE WELLBEING (BODY, MIND & SOUL)</h2>
-          <p>Did you know we take over 20,000 breaths a day? Yet most of us have restricted breathing patterns. When we're not breathing to our full potential, we're also not living to our full potential... since breath is quite literally life.</p>
+          <p>Did you know we take over 20,000 breaths a day? Yet most of us have restricted breathing patterns. When we&apos;'re not breathing to our full potential, we&apos;'re also not living to our full potential... since breath is quite literally life.</p>
           <p>Lets get back to that perfect open breath you were born with, so you can ease any tension or stress, release emotional baggage, increase your clarity and focus, connect more deeply, tune into your intuition and ultimately feel lighter, brighter and fully alive! </p> 
           <p>Change the way you breathe, change the way you live!</p>
 
